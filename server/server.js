fc1f5406a6d5ee543db41aae606da991a1ae6c93
Anthony Schafrik <anthonyschafrik@gmail.com>
2019-02-17 23:21:22 -0500

    Start the app set up

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const log = console.log;
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, '../dist')));
+
+let port = 2020;
+app.listen(port, () => {
+  log(`server is listing on port ${port}`);
+});
