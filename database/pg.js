e30ee7f1818a133566e05143892a42bd0a67f377
unknown <anthonyschafrik@gmail.com>
2019-02-19 18:36:30 -0500

    Create some sql tables

--- database/pg.js
+++ database/pg.js
@@ -1,6 +1,6 @@
 const pg = require('pg');
 
-const conString = 'postgres://postgres:postgres@localhost:5432/PaperDragon';
+const conString = 'postgres://postgres:password@localhost:5432/PaperDragon';
 
 const client = new pg.Client(conString);
 
@@ -9,6 +9,38 @@ client.connect(err => {
     console.log(`An error has happend conecting to pg database; -->: ${err}`);
   } else {
     console.log(`Connected to pg.`);
+  }
+});
+
+let sqlUserAccount =
+  'CREATE TABLE Accounts (id SERIAL primary key, username VARCHAR(15) UNIQUE, firstname VARCHAR(255),  password VARCHAR(15))';
+
+client.query(sqlUserAccount, err => {
+  if (err) {
+    if (err.code === '42P07') {
+      console.log('Accounts Table all ready there');
+    } else {
+      console.log(`Error in database => ${err}`);
+      throw err;
+    }
+  } else {
+    console.log('Accounts Table created');
+  }
+});
+
+let sqlCharacters =
+  'CREATE TABLE Characters ( id SERIAL primary key, userid INT references Accounts(id), name VARCHAR(15) UNIQUE, level INT, hp INT, exp INT)';
+
+client.query(sqlCharacters, err => {
+  if (err) {
+    if (err.code === '42P07') {
+      console.log('Characters Table all ready there');
+    } else {
+      console.log(`Error in database => ${err}`);
+      throw err;
+    }
+  } else {
+    console.log('Characters Table created');
   }
 });
 
