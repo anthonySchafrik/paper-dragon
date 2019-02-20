ad61ff7f2d00487258ad80eb6e8b5e867f46c28e
unknown <anthonyschafrik@gmail.com>
2019-02-20 08:50:27 -0500

    Make monsterSchema that apon call saves to datebase

--- database/mongo.js
+++ database/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const Schema = mongoose.Schema;
 let conString = 'mongodb://localhost/PaperDragon';
 
 let db = mongoose.connection;
@@ -7,3 +7,23 @@ mongoose.connect(conString, { useNewUrlParser: true });
 
 db.on('error', console.error.bind(console, 'mongoDB connection error'));
 db.once('open', () => console.log('mongoDB successfully connected'));
+
+let monsterSchema = new Schema({
+  type: String,
+  element: String,
+  moves: Array
+});
+
+let Monster = mongoose.model('Monster', monsterSchema);
+
+// let fireDragon = new Monster({
+//   type: 'Dragon',
+//   element: 'Fire',
+//   moves: ['Fireball']
+// });
+
+// fireDragon.save((err, fireDragon) => {
+//   if (err) {
+//     console.log(err);
+//   }
+// });
