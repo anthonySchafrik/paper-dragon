bebf84a685dd5d278d5caf053be2fb31cfd2a342
Anthony Schafrik <anthonyschafrik@gmail.com>
2019-02-28 19:48:49 -0500

    Added redux-persist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 
 import App from './components/App';
 
-import store from './reducers';
+import { store, persistor } from './reducers';
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,30 @@
 import { combineReducers } from 'redux';
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 
 import characterInfo from './characterReducer';
 import loginInfo from './loginReducer';
 import signUp from './signUpReducer';
 
-const reducers = combineReducers({ characterInfo, loginInfo, signUp });
+const rootReducer = combineReducers({ characterInfo, loginInfo, signUp });
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+// const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 // const store = createStore(reducers, applyMiddleware(thunk));
 
-export default store;
+let store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+let persistor = persistStore(store);
+
+export { store, persistor };
